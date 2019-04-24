5525a1e1700badf2e3a893a0e7866463dbae61a4
hjio <zhixiang@live.cn>
2019-04-24 18:44:57 +0800

    update

--- src/app.py
+++ src/app.py
@@ -1,12 +1,14 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 
 @app.route('/<pagename>', methods = ['GET'])
 def hello_world(pagename):
     return pagename
-
+    
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=4430)
+

a29e6962f85deb5efa6857ab1c4184d49e17a725
hjio <zhixiang@live.cn>
2019-04-28 16:03:51 +0800

    feat: update login

--- src/api/user.py
+++ src/api/user.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, request, session, make_response
+import secrets
+from werkzeug.security import generate_password_hash, check_password_hash
 import json
 from .db import client
 
@@ -12,17 +14,25 @@ api = Blueprint('user', __name__)
 
 @api.route('/current', methods=['GET'])
 def user_current():
-    email = request.args.get('email')
-
     auth_token = request.cookies.get('auth_token')
+    email = session.get(auth_token)
+    if email:
 
-    return json.dumps({
-        "status": 'UNREGISTERED'
-    })
+        info = users_col.find_one({
+            "email": email
+        })
+
+        return json.dumps({
+            "email": info['email']
+        })
+    else:
+        return make_response(json.dumps({
+            "error": True
+        }))
 
 
 @api.route('/status', methods=['GET'])
-def user_current():
+def user_status():
     email = request.args.get('email')
     if not email:
         return make_response('', 422)
@@ -30,7 +40,7 @@ def user_current():
         user_result = users_col.find({
             "email": email
         })
-        if len(user_result) == 0:
+        if user_result.count() == 0:
             return json.dumps({
                 "status": "UNREGISTERED"
             })
@@ -44,6 +54,18 @@ def user_current():
 def user_login():
     email = request.form['email']
     passwords = request.form['passwords']
+    passwords_hash = users_col.find_one({
+        "email": email
+    })['passwords']
+    if check_password_hash(passwords_hash, passwords):
+        response = make_response(json.dumps({
+            "error": False
+        }))
+        token = secrets.token_hex(64)
+        session[token] = email
+        session.permanent = True
+        response.set_cookie('auth_token', token)
+        return response
 
 
 @api.route('/register', methods=['PUT'])
@@ -51,16 +73,38 @@ def user_register():
     email = request.form['email']
     passwords = request.form['passwords']
     invitation_code = request.form['invitationCode']
-    user_result = users_col.find({
+
+    email_is_available = users_col.find({
         "email": email
-    })
-    inv_code_result = inv_code_col.find({
-        "code": invitation_code
-    }).where("this.times > 0")
+    }).count() == 0
 
-    print(list(user_result))
-    print(list(inv_code_result))
+    if email_is_available:
+        inv_code_result = inv_code_col.find_and_modify({
+            "code": invitation_code,
+            "$where": "this.remaining > 0"
+        }, {
+            "$inc": {"remaining": -1}
+        })
 
-    return json.dumps({
+        if inv_code_result:
+            users_col.insert_one({
+                "email": email,
+                "used_inv_code": invitation_code,
+                "passwords": generate_password_hash(passwords)
+            })
+            return json.dumps({
+                "error": False,
+                "msg": "Account created."
+            })
 
-    })
+        else:
+            return json.dumps({
+                "error": True,
+                "msg": "Your invitation code is wrong or its remaining number has been used up."
+            })
+
+    else:
+        return json.dumps({
+            "error": True,
+            "msg": "This email address has been registered."
+        })
--- src/app.py
+++ src/app.py
@@ -1,23 +1,25 @@
 from flask import Flask, request
 import random
 import string
+import datetime
 
-from .api import user
+from api import user
 
 API_VERSION = 'v1'
 
 app = Flask(__name__)
+app.permanent_session_lifetime = datetime.timedelta(seconds=120*60)
 app.secret_key = ''.join(random.choices(
     string.ascii_letters + string.digits, k=64))
 
 
-def createApiPrefix(append_path):
+def create_api_prefix(append_path):
     print(f"/api/{API_VERSION}{append_path}")
     return f"/api/{API_VERSION}{append_path}"
 
 
 def main():
-    app.register_blueprint(user.api, url_prefix=createApiPrefix('/user'))
+    app.register_blueprint(user.api, url_prefix=create_api_prefix('/user'))
     app.debug = True
     app.run(host='0.0.0.0', port=4430)
 
